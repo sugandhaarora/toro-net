dd336205ae69684a8b94ecd6b8aa1dcd900b0083
Carlos ontiveros <Caontiveros25@gmail.com>
2017-12-02 22:17:24 -0800

    updated bin scripts to include neo4j

--- bin/run-back-end.sh
+++ bin/run-back-end.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-TOPDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"
-mkdir -p $TOPDIR/var/log
 
-echo "starting backend server"
+TOPDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"
+
+if [ ! -f $TOPDIR/.env ] ; then
+  echo .env file is missing
+  exit
+else
+  source $TOPDIR/.env
+fi
+
+mkdir -p $TOPDIR/var/log
+echo "starting backend server on port $PORT"
+echo "=> point your browser to $APP_URL"
+
 cd $TOPDIR
-npm run serve > $TOPDIR/var/log/backend.log
+./node_modules/nodemon/bin/nodemon.js server.js > $TOPDIR/var/log/backend.log
--- bin/run-front-end.sh
+++ bin/run-front-end.sh
@@ -1,18 +1,8 @@
 #!/bin/sh
 
-
 TOPDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"
-
-if [ ! -f $TOPDIR/.env ] ; then
-  echo .env file is missing
-  exit
-else
-  source $TOPDIR/.env
-fi
-
 mkdir -p $TOPDIR/var/log
-echo "starting front-end server on port $PORT"
-echo "=> point your browser to $APP_URL"
 
+echo "starting frontend server"
 cd $TOPDIR
-./node_modules/nodemon/bin/nodemon.js server.js > $TOPDIR/var/log/frontend.log 
+npm run serve > $TOPDIR/var/log/frontend.log
